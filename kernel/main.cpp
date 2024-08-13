8bd99fce76e35cf4ae6bef0f1d72f2c1f31de255
yakishamo <82935423+yakishamo@users.noreply.github.com>
2024-08-13 09:35:44 +0900

    カーネルからフレームバッファ書き換え成功

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -8,6 +8,9 @@ void hlt() {
 extern "C"
 __attribute__((ms_abi))
 int KernelMain(FrameInfo *frame_info){
+	for(uint64_t i = 0; i < frame_info->frame_size; i++) {
+		frame_info->frame_base[i] = 0xff;
+	}
 	hlt();
 	return 0;
 }
